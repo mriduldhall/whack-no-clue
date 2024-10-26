1df673d971d18e2abb8f67008495b2527d1fb6e7
spooky2948 <r.m.barbu1@gmail.com>
2024-10-26 16:31:14 +0100

    change layout from box to anchor

--- Main/BankingAppMain.py
+++ Main/BankingAppMain.py
@@ -1,14 +1,15 @@
 from kivy.app import App
 from kivy.uix.label import Label
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.anchorlayout import AnchorLayout
 
 class MainApp(App):
     def build(self):
-        layout = BoxLayout(orientation = 'horizontal', )
-        balance = 2000.00 #Stores the balance of the user.
+        layout = AnchorLayout(anchor_x = 'center', anchor_y = "top", padding = [0,10,0,0] )
+        balance = 2400.00 #Stores the balance of the user.
   
-        label = Label(text=str(balance),
-                      size_hint=(.5, .5),
-                      pos_hint={'center_x': .5, 'center_y': .5})
+        label = Label(text="Balance: " + str(balance),
+                      size_hint=(.2, .2))
+        
+        layout.add_widget(label)
 
-        return label
+        return layout
