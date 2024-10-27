acbb8392e158796b916518b7934043172e7e4305
spooky2948 <r.m.barbu1@gmail.com>
2024-10-27 05:14:59 +0000

    Update Banking Main :3

--- Main/BankingMain.py
+++ Main/BankingMain.py
@@ -7,6 +7,11 @@ from kivy.uix.widget import Widget
 from kivy.uix.layout import Layout
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.scrollview import ScrollView
+from kivy.effects.scroll import ScrollEffect
+
+class Scroll(ScrollView):
+    pass
 
 class HorizBox(Widget):
     pass
@@ -27,16 +32,37 @@ class rootuiApp(App):
     def build(self):
         database = Connection(host='165.232.37.228',database='whacknoclue',user='whacknoclueuser',password='P@ssword123')
         self.balance = str(database.get_balance())
-        print(RootUI().children)
         return RootUI()
     
     def on_start(self):
         database = Connection(host='165.232.37.228',database='whacknoclue',user='whacknoclueuser',password='P@ssword123')
-        categories_db = database.get_categories()
-        for i in range(len(categories_db)):
-            print(categories_db[i][1])
-            self.root.ids.categories.add_widget(ObtainCategories(text = categories_db[i][1]))
-        print(self.root.ids.categories.children)
+        transactions_db = database.get_transactions()
+        transactions_db.sort(key=lambda x: x[3])
+        transactions_db = list(reversed(transactions_db))
+        len_transactions = len(transactions_db)
+
+        
+        for i in range(len(transactions_db)):
+            print(transactions_db[i][1])
+            self.root.ids.categories.add_widget(ObtainCategories(text = transactions_db[i][1], color = self.set_color(transactions_db[i][5])))
+            self.root.ids.categories.add_widget(ObtainCategories(text = str(transactions_db[i][2]), color = self.set_color(transactions_db[i][5])))
+        # for i in range(len(self.root.ids.categories.children)):
+        #    self.root.ids.categories.children[i].rgba = self.set_color(transactions_db[i][5])
+
+    def set_color(self,color_id):
+        match color_id:
+            case 1:
+                return (0,0,1,1)
+            case 2:
+                return (1,0,0,1)
+            case 3:
+                return (0,1,0,1)
+            case 4:
+                return (1,1,0,1)
+            case 5:
+                return (1,0,1,1)
+            case 6:
+                return (0.5,0.5,0.5,1)
 
 #   database = Connection(host='165.232.37.228',database='whacknoclue',user='whacknoclueuser',password='P@ssword123')
 #        db_categories = database.get_categories()
